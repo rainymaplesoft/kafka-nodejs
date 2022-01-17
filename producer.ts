2ecb2e2282bbf75eec9a95fb9d8234c78b05d079
rainymaple <ljghot@hotmail.com>
2022-01-16 19:14:29 -0500

    add docker-compose for Kafka

--- producer.ts
+++ producer.ts
@@ -13,10 +13,12 @@ const _topic = 'Users'
 let _data = users
 
 const kafka = new Kafka({
-//   brokers: [`${host}:29092`],
+  //   brokers: [`${host}:29092`],
   brokers: [`localhost:29092`],
   clientId: 'nodejs-producer',
 })
+
+setExit()
 
 const producer = kafka.producer()
 
@@ -37,9 +39,9 @@ const sendMessage = () => {
       compression: CompressionTypes.GZIP,
       messages: createMessage(index, _data[index]),
     })
-    .then(data=>{
-      console.log("test---!!!@@@")
-        console.log(data)
+    .then((data) => {
+      console.log('test---!!!@@@')
+      console.log(data)
     })
     .catch((e) => console.error(`[example/producer] ${e.message}`, e))
     .finally(() => {
@@ -53,29 +55,33 @@ const createMessage = (index: number, d: any) => [
     value: JSON.stringify(d),
   },
 ]
-const errorTypes = ['unhandledRejection', 'uncaughtException']
-const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
 
-errorTypes.map((type) => {
-  process.on(type, async () => {
-    try {
-      console.log(`process.on ${type}`)
-      await producer.disconnect()
-      console.log(`producer disconnected normally`)
-      process.exit(0)
-    } catch (_) {
-        console.log(`producer disconnected with error`)
-      process.exit(1)
-    }
+function setExit() {
+  const errorTypes = ['unhandledRejection', 'uncaughtException']
+  const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+  errorTypes.map((type) => {
+    process.on(type, async () => {
+      try {
+        console.log(`producer disconnected normally`)
+        console.log(`process.on ${type}`)
+        await producer.disconnect()
+        process.exit(0)
+      } catch (_) {
+        console.log(`producer disconnected with error ${type}`)
+        process.exit(1)
+      }
+    })
   })
-})
 
-signalTraps.map((type) => {
-  process.once(type, async () => {
-    try {
-      await producer.disconnect()
-    } finally {
-      process.kill(process.pid, type)
-    }
+  signalTraps.map((type) => {
+    process.once(type, async () => {
+      try {
+        console.log(`producer disconnected normally [${type}]`)
+        await producer.disconnect()
+      } finally {
+        console.log(`producer disconnected with error [${type}]`)
+        process.kill(process.pid, type)
+      }
+    })
   })
-})
+}
